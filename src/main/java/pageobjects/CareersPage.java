928f050c9f61eae464fdc15ce198ac2079222da3
User <1234>
2021-09-25 11:08:31 +0300

    first commit

--- src/main/java/pageobjects/CareersPage.java
+++ src/main/java/pageobjects/CareersPage.java
@@ -13,11 +13,19 @@ public class CareersPage {
     SelenideElement keywordField = $(byXpath("//input[@id='new_form_job_search_1445745853_copy-keyword']"));
     SelenideElement locationSelector = $(byXpath("//span[@class='select2-selection__rendered']"));
     SelenideElement skillsField = $(byXpath("//div[@class='selected-params ']"));
+    ElementsCollection twoListsOfSkills = $$(byXpath("//ul[@class='multi-select-column']"));
+    ElementsCollection skills = $$(byXpath("//li[@role='treeitem']"));
     SelenideElement remoteCheckBox = $(byXpath("//input[@id='id-ce1e74aa-e69b-3bd2-9f9f-41628029ec68-remote']"));
     SelenideElement findBtn = $(byXpath("//button[@class='recruiting-search__submit']"));
 
     public void clickLocationSelectorBtn() {
         locationSelectorBtn.click();
+    }
+
+    public void setCountryLocation(String text){
+        for(int i = 0; i < listCountryForLocation.size(); i++){
+//            if(listCountryForLocation.get(i).$(byXpath()))
+        }
     }
 
     public void setKeywordField(String text) {
@@ -28,15 +36,15 @@ public class CareersPage {
         locationSelector.sendKeys(text);
     }
 
-    public void setSkillsField(SelenideElement skillsField) {
-        this.skillsField = skillsField;
+    public void clickSkillsField() {
+        skillsField.click();
     }
 
-    public void setRemoteCheckBox(SelenideElement remoteCheckBox) {
-        this.remoteCheckBox = remoteCheckBox;
+    public void clickRemoteCheckBox() {
+        remoteCheckBox.click();
     }
 
-    public void setFindBtn(SelenideElement findBtn) {
-        this.findBtn = findBtn;
+    public void clickFindBtn() {
+        findBtn.click();
     }
 }
